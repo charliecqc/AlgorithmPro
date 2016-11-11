143012bde77c793f11fb4f38293b8e5b44cdf87e
charlie.cqc@gmail.com <root@localhost.localdomain>
2016-11-11 15:20:47 +0900

    Jury_compromise read input implemented

--- Jury_Compromise/Jury_Compromise.cpp
+++ Jury_Compromise/Jury_Compromise.cpp
@@ -1,18 +1,70 @@
 #include <iostream>
-#include <fstream.h>
+#include <fstream>
 #include <stdlib.h>
+#include <vector>
+#include <map>
+#include <string.h>
+#include <list>
 using namespace std;
-int main (int argc, char **argv)
+/*Global varibles*/
+
+class Pair 
 {
-	char buffer[256];
-	ifstream in("input");
-	if(!in.is_open())
+public:
+	int p;
+	int d;
+	Pair(int a, int b)
 	{
-		exit(1);	
+		p = a;
+		d = b;
 	}
-	while(!in.eof()) {
-		in.getline(buffer, 2)
-		cout << buffer <<endl;
+};
+
+typedef list <class Pair > UDT_LIST_INT_INT;
+UDT_LIST_INT_INT pairs; /*<di, pi>*/
+vector <UDT_LIST_INT_INT> container;/*for store all pairs in each round */
+
+
+int readInput(char *path);
+
+int main ()
+{
+	char *path = "input";
+	int r = readInput(path);
+	UDT_LIST_INT_INT::iterator i;
+	for(i = pairs.begin(); i != pairs.end(); ++i) {
+		cout << i->p << " " << i->d << endl;
 	}
-	return 0;
+	return r;
+}
+
+int readInput(char *path) 
+{
+	int r = 0;
+	fstream file;
+	char buffer[4];
+	int count =0; //count to identify each round
+	file.open(path, ios::in);
+	if(file.fail()) {
+		cout << "file does't exist" << endl;
+		file.close();
+		r = -1;
+		return r;
+	}else {	
+		while(!file.eof()) {
+			file.getline(buffer, 4, '\n');
+			if(!strlen(buffer)) {
+				class Pair one(0,0);
+				pairs.push_back(one);
+				continue;
+			}
+			else {
+				class Pair one(buffer[0] - '0', (int)buffer[2] - '0');
+				pairs.push_back(one);
+			}
+			
+		}
+	}
+	file.close();
+	return r;
 }
